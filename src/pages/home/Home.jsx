ed1ed17ca17fb01fa33f8462345b9a5412146fc8
MuhArul <you@example.com>
2024-04-29 10:16:54 +0700

    first

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,8 +16,8 @@ const Home = () => {
         <Widget type="balance"/>
     </div>
     <div className="charts">
-        <Widget type="order" aspect={2/1}/>
-        <Widget type="earning" aspect={2/1}/>
+        <Chart type="order" aspect={2 / 1}/>
+        <Chart type="earning" aspect={2 / 1}/>
     </div>
     </div>
  </div>
