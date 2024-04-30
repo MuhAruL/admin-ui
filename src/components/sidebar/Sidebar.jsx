2187bade9260706a992af34b0551e4af98013363
MuhArul <you@example.com>
2024-04-30 16:25:18 +0700

    tugas syarat UTS

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,11 @@
 import "./sidebar.scss"
 import { Link } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import PaymentIcon from '@mui/icons-material/Payment';
+import StoreIcon from '@mui/icons-material/Store';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar = () => {
   return (
@@ -21,22 +26,27 @@ const Sidebar = () => {
       <p className="title">LIST</p>
       <Link to="/users">
         <li>
+        <PersonIcon className="icon"/>
           <span>Users</span>
         </li>
       </Link>
       <Link to="/products">
         <li>
+        <PaymentIcon className="icon"/>
           <span>Products</span>
         </li>
       </Link>
       <li>
+      <StoreIcon className="icon"/>
         <span>Orders</span>
       </li>
       <p className="title">USER</p>
       <li>
+      <AccountCircleOutlinedIcon className="icon"/>
         <span>Profile</span>
       </li>
       <li>
+      <LogoutOutlinedIcon className="icon"/>
         <span>Logout</span>
       </li>
     </ul>
